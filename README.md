82cfc0bfd1b64fa24fd831b57ed952d7d633508b
Ignacio911 <Ignacio911@gmail.com>
2018-05-30 07:12:00 +0000

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# https://github.com/Ignacio911/assetsls2h.pythonanywhere.com/blob/master/sitemap.xml.gz?raw=true
+# https://assetsls2h.pythonanywhere.com/sitemap.xml.gz
---
+++ pinglist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsls2h.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsls2h.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsls2h.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsls2h.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsls2h.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsls2h.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsls2h.netlify.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsls2h.netlify.com/sitemap.xml.gz
